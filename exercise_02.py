36d935dcacabb7e77a484c1d49b6cf1420cced9a
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-19 11:30:37 -0300

    (ADD) New exercise added

---
+++ exercise_02.py
@@ -0,0 +1,6 @@
+    # Create a program that the user enters two values and return the sum of them
+number_one = int(input("Enter any number: "))
+number_two = int(input("Enter another number: "))
+print(number_one)
+print(number_two)
+print(number_one + number_two)
